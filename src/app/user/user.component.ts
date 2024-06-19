ec1b4afb30144b8a21c6357e1e39e9d31f7cfa08
Ali Chavoshi <alichavoshii1372@gmail.com>
2024-06-19 12:47:34 +0330

    add some new avatars in to the project

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-
+import { DUMMY_USERS } from './dummy-users';
+const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
 @Component({
   selector: 'app-user',
   standalone: true,
@@ -7,4 +8,7 @@ import { Component } from '@angular/core';
   templateUrl: './user.component.html',
   styleUrl: './user.component.css',
 })
-export class UserComponent {}
+export class UserComponent {
+  users = DUMMY_USERS;
+  selectedUser = DUMMY_USERS[randomIndex];
+}
