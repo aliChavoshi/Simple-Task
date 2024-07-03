fac686c84960d7b1265025016f27dac9db07e10c
Ali Chavoshi <alichavoshii1372@gmail.com>
2024-07-03 10:26:15 +0330

    add task component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,19 +4,20 @@ import { HeaderComponent } from './header/header.component';
 import { UserComponent } from './user/user.component';
 import { DUMMY_USERS, IUser } from './user/dummy-users';
 import { TasksComponent } from './tasks/tasks.component';
+// import { NgFor, NgIf } from '@angular/common';
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet, HeaderComponent, UserComponent, TasksComponent],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css',
+   selector: 'app-root',
+   standalone: true,
+   imports: [RouterOutlet, HeaderComponent, UserComponent, TasksComponent],
+   templateUrl: './app.component.html',
+   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  selectedUser = signal<IUser>({ avatar: '', id: '', name: '' });
-  users = signal<IUser[]>(DUMMY_USERS);
+   selectedUser = signal<IUser>({ avatar: '', id: '', name: '' });
+   users = signal<IUser[]>(DUMMY_USERS);
 
-  onSelectUser(userId: string) {
-    this.selectedUser.set(this.users().find((user) => user.id === userId));
-  }
+   onSelectUser(userId: string) {
+      this.selectedUser.set(this.users().find((user) => user.id === userId));
+   }
 }
--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -1,12 +1,13 @@
 import { Component, input } from '@angular/core';
+import { TaskComponent } from './task/task.component';
 
 @Component({
-  selector: 'app-tasks',
-  standalone: true,
-  imports: [],
-  templateUrl: './tasks.component.html',
-  styleUrl: './tasks.component.css',
+   selector: 'app-tasks',
+   standalone: true,
+   templateUrl: './tasks.component.html',
+   styleUrl: './tasks.component.css',
+   imports: [TaskComponent]
 })
 export class TasksComponent {
-  name = input.required<string>();
+   name = input.required<string>();
 }
