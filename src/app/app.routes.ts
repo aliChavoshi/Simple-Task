c4f5ed9c33f46dfb5beea7ef4c11a2af39c2d0b9
Ali Chavoshi <alichavoshii1372@gmail.com>
2024-07-06 12:01:07 +0330

    Add new task component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,4 @@
 import { Routes } from '@angular/router';
+import { NewTaskComponent } from './tasks/new-task/new-task.component';
 
 export const routes: Routes = [];
--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -2,21 +2,26 @@ import { Component, OnChanges, SimpleChanges, input, signal } from '@angular/cor
 import { TaskComponent } from './task/task.component';
 import { IUser } from '../user/users.model';
 import { ITask, dummyTasks } from './tasks.model';
+import { NewTaskComponent } from "./new-task/new-task.component";
 
 @Component({
-   selector: 'app-tasks',
-   standalone: true,
-   templateUrl: './tasks.component.html',
-   styleUrl: './tasks.component.css',
-   imports: [TaskComponent]
+    selector: 'app-tasks',
+    standalone: true,
+    templateUrl: './tasks.component.html',
+    styleUrl: './tasks.component.css',
+    imports: [TaskComponent, NewTaskComponent]
 })
 export class TasksComponent implements OnChanges {
+   allTasks = signal<ITask[]>(dummyTasks);
    user = input.required<IUser>();
    taskOfUser = signal<ITask[]>([]);
-   tasks = signal<ITask[]>(dummyTasks);
+   isAddedNewTask = false;
 
    ngOnChanges(changes: SimpleChanges): void {
-      const tasks = this.tasks().filter((t) => t.userId === this.user().id);
+      const tasks = this.allTasks().filter((t) => t.userId === this.user().id);
       this.taskOfUser.set(tasks);
+   }
+   onStartAddTask() {
+      this.isAddedNewTask = true;
    }
 }
