b8af06b870ecce248c0f99c4cc69c40bd9a2b20f
Ali Chavoshi <alichavoshii1372@gmail.com>
2024-07-06 12:24:11 +0330

    fix close btn

--- src/app/tasks/new-task/new-task.component.ts
+++ src/app/tasks/new-task/new-task.component.ts
@@ -1,12 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, input, output } from '@angular/core';
 
 @Component({
-  selector: 'app-new-task',
-  standalone: true,
-  imports: [],
-  templateUrl: './new-task.component.html',
-  styleUrl: './new-task.component.css'
+   selector: 'app-new-task',
+   standalone: true,
+   imports: [],
+   templateUrl: './new-task.component.html',
+   styleUrl: './new-task.component.css'
 })
 export class NewTaskComponent {
+   isOpened = input.required<boolean>();
+   onCancelAddTask = output<boolean>();
 
+   closeDialog() {
+      this.onCancelAddTask.emit(false);
+   }
 }
--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -2,26 +2,26 @@ import { Component, OnChanges, SimpleChanges, input, signal } from '@angular/cor
 import { TaskComponent } from './task/task.component';
 import { IUser } from '../user/users.model';
 import { ITask, dummyTasks } from './tasks.model';
-import { NewTaskComponent } from "./new-task/new-task.component";
+import { NewTaskComponent } from './new-task/new-task.component';
 
 @Component({
-    selector: 'app-tasks',
-    standalone: true,
-    templateUrl: './tasks.component.html',
-    styleUrl: './tasks.component.css',
-    imports: [TaskComponent, NewTaskComponent]
+   selector: 'app-tasks',
+   standalone: true,
+   templateUrl: './tasks.component.html',
+   styleUrl: './tasks.component.css',
+   imports: [TaskComponent, NewTaskComponent]
 })
 export class TasksComponent implements OnChanges {
    allTasks = signal<ITask[]>(dummyTasks);
    user = input.required<IUser>();
    taskOfUser = signal<ITask[]>([]);
-   isAddedNewTask = false;
+   isAddedNewTask = signal(false);
 
    ngOnChanges(changes: SimpleChanges): void {
       const tasks = this.allTasks().filter((t) => t.userId === this.user().id);
       this.taskOfUser.set(tasks);
    }
    onStartAddTask() {
-      this.isAddedNewTask = true;
+      this.isAddedNewTask.set(true);
    }
 }
