56b0508652ba5acdcde9be23cc970c278ba97c4b
Ali Chavoshi <alichavoshii1372@gmail.com>
2024-07-06 12:39:38 +0330

    two way bindings

--- src/app/tasks/new-task/new-task.component.ts
+++ src/app/tasks/new-task/new-task.component.ts
@@ -1,17 +1,25 @@
 import { Component, input, output } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
    selector: 'app-new-task',
    standalone: true,
-   imports: [],
+   imports: [FormsModule],
    templateUrl: './new-task.component.html',
    styleUrl: './new-task.component.css'
 })
 export class NewTaskComponent {
    isOpened = input.required<boolean>();
    onCancelAddTask = output<boolean>();
+   //create new a task
+   title = '';
+   dueDate = '';
+   summary = '';
 
    closeDialog() {
       this.onCancelAddTask.emit(false);
+   }
+   createNewTask() {
+      throw new Error('Method not implemented.');
    }
 }
